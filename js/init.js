c2d210c14e2797481ec700907f1dee89e5f4738f
Jackson Truong <truong.jackson108@gmail.com>
2016-05-25 02:36:20 -0400

    cool changes started animation

--- js/init.js
+++ js/init.js
@@ -1,1 +1,16 @@
-$('#sidebar').affix({});
+$(document).ready(function () {
+	 nodePosition(".character");
+	 nodePosition(".planet-1");
+	 $("#archedName").arctext({radius: 300});
+});
+$(window).resize(function () {
+	nodePosition(".character");
+	nodePosition(".planet-1");
+});
+
+function nodePosition(nodeName){
+	var panelWidth = $("#mainPanel").width();
+	var node = $(nodeName);
+	console.log(panelWidth);
+	node.css('right', (panelWidth/2)-(node.width()/2)+14);
+}
