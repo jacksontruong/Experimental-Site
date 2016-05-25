57cb9c33d4833e2f60adb2c3553358fd1ef012ed
Jackson Truong <truong.jackson108@gmail.com>
2016-05-25 03:26:56 -0400

    Minor Changes

--- js/init.js
+++ js/init.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
 	 nodePosition(".character");
 	 nodePosition(".planet-1");
+	 planetText();
 	 $("#archedName").arctext({radius: 300});
 });
 $(window).resize(function () {
 	nodePosition(".character");
 	nodePosition(".planet-1");
+	planetText();
 });
 
 function nodePosition(nodeName){
 	var panelWidth = $("#mainPanel").width();
 	var node = $(nodeName);
-	console.log(panelWidth);
 	node.css('right', (panelWidth/2)-(node.width()/2)+14);
+}
+
+function planetText(){
+	var planetWidth = $(".planet").width();
+	var node = $(".planet-core");
+	node.css('margin-top', (planetWidth/2)-(node.height()/2)+14);
 }
